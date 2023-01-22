78f5faaf5257c339c484e59d11b710a2d59f981b
PGA <paulgadam@gmail.com>
2023-01-22 09:46:38 -0700

    updated Front end display headings

--- src/features/FrontDisplay/FrontEndDisplay.js
+++ src/features/FrontDisplay/FrontEndDisplay.js
@@ -1,10 +1,10 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+import DisplayCard from './FrontCard';
 import { selectFeaturedGuitar } from '../guitar/guitarSlice';
 import { selectFeaturedMulti } from '../multiguitar/multiGuitarSlice';
 //Used to map DisplayCards for HomePage...
 
-const DisplayList = () => {
+const FrontEndDisplay = () => {
     const items = [selectFeaturedGuitar(), selectFeaturedMulti(),selectFeaturedMulti()];
     //mapping from CAMPSITES.js data array and PROMOTIONS.js data array...1 from each on Homepage
 
@@ -21,4 +21,4 @@ const DisplayList = () => {
     );
 };
 
-export default DisplayList;
+export default FrontEndDisplay;
