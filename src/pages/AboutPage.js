8b33e657806e7af23bf31fbfd98fa0b0a0621fca
PGA <paulgadam@gmail.com>
2023-01-22 07:10:23 -0700

    updated card css for about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -24,10 +24,10 @@ const AboutPage = () => {
             <Row>
                 <Col>
                     <Card>
-                        <CardHeader className=" mt-1 bg text-left" style={{  border: "5px ridge #e6e6e6", borderRadius: "2%", backgroundColor: "LightGrey", color: "Blue",padding: "12px", fontSize: "60px", fontFamily: "lobster"}}>
+                        <CardHeader className=" mt-1 bg text-left" style={{  border: "5px ridge #e6e3fa", borderRadius: "2%", backgroundColor: "LightGrey", color: "Blue",padding: "12px", fontSize: "60px", fontFamily: "lobster"}}>
                            About the Author
                         </CardHeader>
-                        <CardBody className="bg" style={{ border: "5px ridge #e6e6e6",backgroundColor: "LightSteelBlue", color: "SkyBlue",padding: "6px", fontSize: "45px",fontFamily: "Lucida Handwriting"}}>
+                        <CardBody className="bg" style={{ border: "5px ridge #e6e3ea",backgroundColor: "LightSteelBlue", color: "SkyBlue",padding: "6px", fontSize: "45px",fontFamily: "Lucida Handwriting"}}>
                             Body for my guitar card and such
                             <p> asdlfkajsdflkajsdfasdf</p>
                             <p> asdlfkajsdflkajsdfasdf</p>
