9305131c3d2d818217a5ce783adfb450da1bff3d
PGA <paulgadam@gmail.com>
2023-01-23 04:20:00 -0700

    carousel's suck pretty bad

--- src/features/FrontDisplay/FronEndCarouselDisplay.js
+++ src/features/FrontDisplay/FronEndCarouselDisplay.js
@@ -1,11 +1,20 @@
 import { Col, Row } from 'reactstrap';
-import {UnCarousel} from './FrontCarousel'; 
+import UnCarousel from './FrontCarousel'; 
 
 const FrontEndCarouselDisplay = () => {
 
     return (
-        <Row>
+        <Row >
+
             <UnCarousel />
+            <Col>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            
+            </Col>
         </Row>
     );
 };
--- src/features/FrontDisplay/FrontCarousel.js
+++ src/features/FrontDisplay/FrontCarousel.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import { UncontrolledCarousel, Col } from 'reactstrap';
 
-import {CAROUSEL} from '../../app/shared/CAROUSEL';
-
-export const selectAllCarousel = () => {
-    return CAROUSEL;
-};
-const UnCarousel = (items) => {
-const items = selectAllCarousel();
-    return(
+//import {CAROUSEL} from '../../app/shared/CAROUSEL';
+const items = [
+    {
+        src: '../assets/img/p_region_RG5170G_SVF_00_01.png',
+        altText: '5170',
+        caption: 'RG 5170',
+        header: 'RG5170'
+    },
+    {
+        src: '../assets/img/p_region_RG5170G_SVF_00_01.png',
+        altText: '5170',
+        caption: 'RG 5170',
+        header: 'RG5170'
+    },
+    {
+        src: '../assets/img/p_region_RG5170G_SVF_00_01.png',
+        altText: '5170',
+        caption: 'RG 5170',
+        header: 'RG5170'
+    }
+];
+// export const selectAllCarousel = () => {
+//     return CAROUSEL;
+// };
+const UnCarousel = () => {
+    return (
+        <Col>
         <UncontrolledCarousel items={items} />
-        );
+        </Col>)
 };
+// const items = selectAllCarousel();
+//     return(
+//         <UncontrolledCarousel items={items} />
+//         );
+// };
 export default UnCarousel;
