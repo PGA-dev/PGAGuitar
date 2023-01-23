d5cb4e2c78560afef27aabc6c6da23b9906f6941
PGA <paulgadam@gmail.com>
2023-01-23 04:47:51 -0700

    updated too much...lol

--- src/features/bassstats/BassStat.js
+++ src/features/bassstats/BassStat.js
@@ -1,11 +1,14 @@
 
 const BassStat = ({bassstat}) => {
     const { id, text, heading } = bassstat;
+    if (bassstat){
     return (
-        <p>
-            {heading}: {text}
+        <p style={{padding:'2px', fontFamily: 'papyrus', color: 'blueviolet'}} >
+            {heading}:
+            <div style={{padding:'2px', fontFamily: 'papyrus', color: 'aqua'}}>{text}</div>
         </p>
-    );
+    );};
+    return null;
 };
 
 export default BassStat;
