b86bedc5a3ad5c1bf01dc0497467c5dd5019f53d
PGA <paulgadam@gmail.com>
2023-01-23 04:05:37 -0700

    rewired homepage to run with slightly different setup pre-carousel

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import { Container } from 'reactstrap';
-import DisplayList from '../features/FrontDisplay/FrontEndDisplay';
+import FrontEndDisplay from '../features/FrontDisplay/FrontEndDisplay';
 import SubHeader from '../components/SubHeader';
+
 
 const HomePage = () => {
     return (
         <Container>
             <SubHeader current='Home' />
-            <DisplayList />
+            <FrontEndDisplay />
         </Container>
     );
 };
