91b234e6f7d779d60f45e01def2d7c420e94d450
PGA <paulgadam@gmail.com>
2023-02-12 23:47:51 -0700

    updated carousel and all to current as per private...fixed printer image adjust

--- src/features/Contact/ContactForm.js
+++ src/features/Contact/ContactForm.js
@@ -19,6 +19,7 @@ const ContactForm = () => {
         console.log('form values:', values);
         console.log('JSON format:', JSON.stringify(values));
         dispatch(postContact(contact));
+        alert(`Your Contact info and Feedback has been recieved`);
         resetForm();
         
     };
